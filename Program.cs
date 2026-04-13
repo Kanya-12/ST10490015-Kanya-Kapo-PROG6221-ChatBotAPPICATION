be507f50ff7c7790695146b234a6c42bc36e3c85
Kanya-12 <ST10490015@rcconnect.edu.za>
2026-04-13 02:55:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CyberSecurityChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "KanyaShield Chatbot";
+
+            AudioPlayer.PlayGreeting();
+
+            Logo.Display();
+
+            ChatBot bot = new ChatBot();
+            bot.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
